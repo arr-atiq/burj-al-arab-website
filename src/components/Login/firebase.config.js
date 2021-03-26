41b627e685b2ab232d48a2df6d326d1921c88f38
aar-atiq <aar.msdeveloper01@gmail.com>
2021-03-27 04:16:06 +0600

    initialize burj-al-arab

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCjAzdKbzLhXCRo7OKPXOXQ5GHmKXL0Gmc",
+  authDomain: "burj-al-arab-website-abadf.firebaseapp.com",
+  projectId: "burj-al-arab-website-abadf",
+  storageBucket: "burj-al-arab-website-abadf.appspot.com",
+  messagingSenderId: "396352407290",
+  appId: "1:396352407290:web:7eb3928347fd0e5e486b43"
+};
+export default firebaseConfig;
